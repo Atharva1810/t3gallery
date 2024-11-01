198d08c0a8ddbb725173d6871ca48de655b66aed
Atharva <atharvadere1018@gmail.com>
2024-11-01 10:31:46 +0530

    Connected database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
-  const mockUrls = [
-    "https://utfs.io/f/S23XiUkZcldXK6hc8FU4uDXEVBCqZygKvLsJdRT9rHW5Iect",
-    "https://utfs.io/f/S23XiUkZcldXOEUEmUIQpwEeIjMFVoD5AnR3tiNlU46SGmgT",
-    "https://utfs.io/f/S23XiUkZcldXaodvsDYuxVoHFJURsOSzCdagnGWkI8hQE2lj",
-    "https://utfs.io/f/S23XiUkZcldXrahLTp9m1ADR3sEfbGj4XQHSIPw82UkTLJv6",
-    "https://utfs.io/f/S23XiUkZcldXUMew4H9jV02JgEZPskmNuboqSvcQnTw7Aj1B",
-  ];
-
-  const mockImages = mockUrls.map((url, index) => {
-    return { id: index + 1, url };
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
   });
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col gap-2">
             <img src={image.url} alt="image" />
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
