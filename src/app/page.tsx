11b1d9ab3c4d3de33adcdf3e99a47d4a24e1d789
Atharva <atharvadere1018@gmail.com>
2024-11-01 11:57:37 +0530

    Add auth to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,31 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
-
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col gap-2">
+          <img src={image.url} alt="image" />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col gap-2">
-            <img src={image.url} alt="image" />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <p className="h-full w-full text-center text-2xl">
+          Please sign in to view your images
+        </p>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
