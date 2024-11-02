62847cf06e8d4203dbb2002ab6d88a950ae6832a
Atharva <atharvadere1018@gmail.com>
2024-11-02 10:55:09 +0530

    Link images and users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4">
       {images.map((image) => (
